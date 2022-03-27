1422be376f7cfe5aae2d9f67c01edda3549586c2
Sonic25555 <79093705+Sonic25555@users.noreply.github.com>
2022-03-27 17:28:51 -0400

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,46 @@
+img="";
+status = "";
+object=[];
+function preload(){
+    img=loadImage("https://cars.usnews.com/static/images/Auto/custom/14737/2022_Acura_ILX_1.jpg");
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Object";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        object=results;
+
+    }
+}
+function draw(){
+    image(img,0,0,380,380);
+    if(status!=""){
+        objectDetector.detect(img,gotResult);
+        for(i=0; i<object.length; i++){
+            document.getElementById("status").innerHTML="Status : Object Detected";
+            document.getElementById("no_of_objects").innerHTML="Number of objects detected : "+object.length;
+            fill("#FF0000");
+            percent=floor(object[i].confidence*100);
+            text(object[i].label+" "+percent+"%",object[i].x,object[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+    }
+}
+function back(){
+    window.location = "index.html";
+    }
+    
